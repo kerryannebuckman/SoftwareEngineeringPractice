8de5c7dacd451411886b90517328911ca9480923
kerrybuckman <kbuckman@ithaca.edu>
2020-01-22 13:38:29 -0500

    isEmailValid test updated

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -25,6 +25,57 @@ class BankAccountTest {
     void isEmailValidTest(){
         assertTrue(BankAccount.isEmailValid( "a@b.com"));
         assertFalse( BankAccount.isEmailValid(""));
+
+
+    //prefix
+        //dashes valid
+        assertTrue(BankAccount.isEmailValid("abc-d@mail.com"));
+
+        //dashes invalid (not followed by letter or number)
+        assertFalse(BankAccount.isEmailValid("abc-@mail.com"));
+
+        //underscores valid
+        assertTrue(BankAccount.isEmailValid("abc_def@mail.com"));
+
+        //underscores invalid (not followed by letter or number)
+        assertFalse(BankAccount.isEmailValid("abc_@mail.com"));
+
+        //periods valid
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.com"));
+
+        //periods invalid (not followed by letter or number)
+        assertFalse(BankAccount.isEmailValid("abc.@mail.com"));
+
+        //special symbols invalid
+        assertFalse(BankAccount.isEmailValid("abc*de@mail.com"));
+        assertFalse(BankAccount.isEmailValid("abc@de@mail.com"));
+        assertFalse(BankAccount.isEmailValid("abc(de@mail.com"));
+        assertFalse(BankAccount.isEmailValid("abc%de@mail.com"));
+        assertFalse(BankAccount.isEmailValid("abc$de@mail.com"));
+
+    //domain
+
+        //special characters invalid
+        assertFalse(BankAccount.isEmailValid("abc@mai%l.com"));
+        assertFalse(BankAccount.isEmailValid("abc@mai^l.com"));
+        assertFalse(BankAccount.isEmailValid("abc@mai*l.com"));
+        assertFalse(BankAccount.isEmailValid("abc@mai*l.com"));
+
+        //valid end of domain (two or more characters after ".")
+        assertTrue(BankAccount.isEmailValid("abcdef@mail.com"));
+        assertTrue(BankAccount.isEmailValid("abcdef@mail.co"));
+        assertTrue(BankAccount.isEmailValid("abcdef@mail.net"));
+
+        //invalid end of domain (less than two characters after ".")
+        assertFalse(BankAccount.isEmailValid("abcdef@mail.c"));
+        assertFalse(BankAccount.isEmailValid("abcdef@mail."));
+        assertFalse(BankAccount.isEmailValid("abcdef@mail.l"));
+
+        //needs period after @
+        assertTrue(BankAccount.isEmailValid("abcdef@mail.com"));
+        assertFalse(BankAccount.isEmailValid("abcdef@mailcom"));
+
+
     }
 
     @Test
